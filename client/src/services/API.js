d5f7aa65e1613d80a687481f1319cbf6587ddc1b
Akshar22Dubey <126651582+Akshar22Dubey@users.noreply.github.com>
2024-08-08 23:26:45 +0530

    Update API.js

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://bitblood-backend-7.onrender.com/api/v1",
+    "https://bitblood-100.onrender.com/api/v1",
 });
 
 API.interceptors.request.use((req) => {
